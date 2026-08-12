b763aa2ea2f5855caf8ef2aeada10b0594c88310
Tsuki <feliperafaelsouza2009@gmail.com>
2026-08-12 09:46:14 -0300

    Add files via upload

---
+++ App_Pam/components/Home.js
@@ -0,0 +1,14 @@
+import {React, useState} from "react-native"
+import {Text, View, Image}
+export default  function Home() {
+    return 
+    (const {nome, setnome} = useState('');
+        <>
+            <Text>Nome</Text>
+            <Textimput style={
+                BorderRadius: 5,
+                BorderWidth
+            }>
+        </>
+    )
+}
